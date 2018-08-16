197fb575bd2e485f4ae0c421f30c15fec6651445
Alvaro Cantador <4078365+alvarocantador@users.noreply.github.com>
2018-08-15 21:44:09 -0300

    Updated LocalActionServicesLoader to follow previous code standarts

--- Protocol/LocalActionServicesLoader.cs
+++ Protocol/LocalActionServicesLoader.cs
@@ -15,27 +15,27 @@ namespace Protocol
 
         public LocalActionServicesLoader()
         {
-            var fileEntries = Directory.GetFiles(PLUGINS_FOLDER_PATH);
+            string[] fileEntries = Directory.GetFiles(PLUGINS_FOLDER_PATH);
 
             foreach (string fileEntry in fileEntries)
             {
-                var assembly = Assembly.LoadFrom(fileEntry);
+                Assembly assembly = Assembly.LoadFrom(fileEntry);
 
                 foreach (Type type in assembly.GetTypes())
                 {
-                    var serviceAttr = (ServiceActionAttribute)type.GetCustomAttribute(typeof(ServiceActionAttribute));
+                    ServiceActionAttribute serviceAttr = (ServiceActionAttribute)type.GetCustomAttribute(typeof(ServiceActionAttribute));
 
                     if (serviceAttr != null)
                     {
-                        var serviceName = (serviceAttr.Name != null) ? serviceAttr.Name : type.Name;
+                        string serviceName = (serviceAttr.Name != null) ? serviceAttr.Name : type.Name;
 
                         foreach (var methodInfo in type.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance))
                         {
                             if (methodInfo.DeclaringType.Equals(type))
                             {
-                                var serviceMethod = (ServiceMethodAttribute)methodInfo.GetCustomAttribute(typeof(ServiceMethodAttribute));
-                                var actionName = serviceMethod != null && serviceMethod.Name != null ? serviceMethod.Name : methodInfo.Name;
-                                var actionIndex = $"{serviceName}.{actionName}";
+                                ServiceMethodAttribute serviceMethod = (ServiceMethodAttribute)methodInfo.GetCustomAttribute(typeof(ServiceMethodAttribute));
+                                string actionName = serviceMethod != null && serviceMethod.Name != null ? serviceMethod.Name : methodInfo.Name;
+                                string actionIndex = $"{serviceName}.{actionName}";
 
                                 ActionCache[actionIndex] = (type: type, method: methodInfo);
                             }
